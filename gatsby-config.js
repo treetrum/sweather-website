f6bbc2d943fde8ec3ecdf12fbce5b80246ecfecd
Sam Davis <sam@sjd.co>
2020-06-27 19:19:59 +1000

    build fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,17 +18,14 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `Sweather 2`,
+                short_name: `sweather-2`,
                 start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
+                background_color: `#fff`,
+                theme_color: `#fff`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                icon: `src/images/icon.png`, // This path is relative to the root of the site.
             },
         },
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
     ],
 };
